173fd0842b7d418f2631f7df49bd5cc824a196b7
Sai Kaushik Sadu <91894475+SaiKaushikSadu@users.noreply.github.com>
2023-09-27 02:44:58 +0530

    contact done

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import Admin from "./Pages/Admin/Admin";
 import AdminLocality from "./Pages/AdminLocality/AdminLocality";
 import AdminApt from "./Pages/AdminApt/AdminApt";
 import Login from "./Pages/Login/Login";
+import ContactUs from "./Pages/ContactUs/ContactUs";
 
 const App = () => {
   const [loading, setLoading] = useState(false);
@@ -44,6 +45,7 @@ const App = () => {
           <Route path="/adminlocal" element={<AdminLocality></AdminLocality>}></Route>
           <Route path="/adminapt" element={<AdminApt></AdminApt>}></Route>
           <Route path="/login" element={<Login/>} />  
+          <Route path="/contact" element={<ContactUs/>} />  
         </Routes>
         <Foot />
       </Router>
