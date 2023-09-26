defa6503b17975adf75e6b3651a1604fc1384923
Sai Kaushik Sadu <91894475+SaiKaushikSadu@users.noreply.github.com>
2023-09-27 02:29:26 +0530

    adminapt done

--- client/src/Pages/AdminApt/AdminApt.js
+++ client/src/Pages/AdminApt/AdminApt.js
@@ -6,17 +6,17 @@ const AdminApt = () => {
     return (
         <>
 
-            <h2 style={{ textAlign: "center", margin: 50 }}>Allocating Locality to Locality collector</h2>
+            <h2 style={{ textAlign: "center", marginTop: 70, marginBottom: 50 }}>Allocating Apartment to Apartment collector</h2>
             <div className='grid-aptc'>
                 <div className="container-aptc">
                     <img className='img-aptc' alt="building" src={avatar} />
                     <div className="heading-aptc">
                         Name : Sagar Singh
                         <select style={{ padding: 7, borderRadius: 30, margin: 10 }}>
-                            <option>Apt 1</option>
-                            <option>Apt 2</option>
-                            <option>Apt 3</option>
-                            <option>Apt 4</option>
+                            <option>S&M Home</option>
+                            <option>Sun Darshan</option>
+                            <option>Elite Tower</option>
+                            <option>Panchasheel</option>
                         </select>
                         <button className='btn-aptc'>
                             Submit
@@ -28,10 +28,10 @@ const AdminApt = () => {
                     <div className="heading-aptc">
                         Name : Vivek
                         <select style={{ padding: 7, borderRadius: 30, margin: 10 }}>
-                            <option>Apt 1</option>
-                            <option>Apt 2</option>
-                            <option>Apt 3</option>
-                            <option>Apt 4</option>
+                            <option>S&M Home</option>
+                            <option>Sun Darshan</option>
+                            <option>Elite Tower</option>
+                            <option>Panchasheel</option>
                         </select>
                         <button className='btn-aptc'>
                             Submit
@@ -43,10 +43,10 @@ const AdminApt = () => {
                     <div className="heading-aptc">
                         Name : Jagjeet Singh
                         <select style={{ padding: 7, borderRadius: 30, margin: 10 }}>
-                            <option>Apt 1</option>
-                            <option>Apt 2</option>
-                            <option>Apt 3</option>
-                            <option>Apt 4</option>
+                            <option>S&M Home</option>
+                            <option>Sun Darshan</option>
+                            <option>Elite Tower</option>
+                            <option>Panchasheel</option>
                         </select>
                         <button className='btn-aptc'>
                             Submit
