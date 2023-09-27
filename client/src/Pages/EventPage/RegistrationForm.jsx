5177fc41e0a956b6e3b79ae55b3aa5bb46c69c3b
Nerd <sssagarbro@gmail.com>
2023-09-27 17:45:36 +0530

    Suraj Lavde Ka Form Change

--- client/src/Pages/EventPage/RegistrationForm.jsx
+++ client/src/Pages/EventPage/RegistrationForm.jsx
@@ -67,7 +67,42 @@ const RegistrationForm = ({ onClose }) => {
             />
             {formErrors.email && <p className="error">{formErrors.email}</p>}
           </div>
-          <button  type="submit" className="submit-button">
+          <div className="form-group">
+            <label>Phone Number</label>
+            <input
+              type="text"
+              name="phone"
+              value={formData.phone}
+              onChange={handleInputChange}
+            />
+            {formErrors.phone && <p className="error">{formErrors.phone}</p>}
+          </div>
+          <div className="form-group">
+            <label>Select Event</label>
+            <select
+              name="event"
+              value={formData.event}
+              onChange={handleInputChange}
+            >
+              <option value="city_cleanup">City Cleanup</option>
+              <option value="tree_plantation">Tree Plantation</option>
+              <option value="recycling_drive">Recycling Drive</option>
+            </select>
+            {formErrors.event && <p className="error">{formErrors.event}</p>}
+          </div>
+          <div className="form-group">
+            <label>Comments (Optional)</label>
+            <textarea
+              name="comments"
+              value={formData.comments}
+              onChange={handleInputChange}
+              rows="4"
+            ></textarea>
+            {formErrors.comments && (
+              <p className="error">{formErrors.comments}</p>
+            )}
+          </div>
+          <button type="submit" className="submit-button">
             Submit
           </button>
         </form>
